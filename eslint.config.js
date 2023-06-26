613c6dcb386cd7ec3fd78c992734acac6d812df7
Vespion <21206333+Vespion@users.noreply.github.com>
2023-06-26 16:37:09 +0100

    ci: setup initial ci workflows

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
 import error from "eslint-plugin-new-with-error";
 import deprecation from "eslint-plugin-deprecation";
-import woke from "eslint-plugin-woke";
 
 import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
@@ -12,22 +11,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname
+	baseDirectory: __dirname,
 });
 
 export default [
 	js.configs.recommended,
 	{
 		languageOptions: {
-			sourceType: "module"
+			sourceType: "module",
 		},
 		plugins: {
-			woke,
-			error
+			error,
 		},
-		rules: {
-			"woke/all": "warn"
-		}
 	},
 	...compat.extends("plugin:regexp/recommended"),
 	...compat.extends("plugin:security/recommended"),
@@ -36,20 +31,20 @@ export default [
 	{
 		files: ["**/*.ts"],
 		plugins: {
-			deprecation
+			deprecation,
 		},
 		rules: {
-			"deprecation/deprecation": "warn"
+			"deprecation/deprecation": "warn",
 		},
 		languageOptions: {
 			sourceType: "module",
 			parser: tsParser,
 			parserOptions: {
 				ecmaVersion: "latest",
-				project: ["./tsconfig.json"]
-			}
-		}
+				project: ["./tsconfig.json"],
+			},
+		},
 	},
 	...compat.extends("plugin:json-schema-validator/recommended"),
-	...compat.extends("prettier")
+	...compat.extends("prettier"),
 ];
